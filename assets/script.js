bfb551bc71951e24cc0a71cf351121904859acbb
Roberts-NewYork <130010476+Roberts-NewYork@users.noreply.github.com>
2023-04-24 13:58:23 -0400

    working on functions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+function listTopics() { 
 for (var x = 0; x < topics.length; x++) { 
     console.log(topics[x]);
+  }
 }
 
-/*
-var topic = "HTML";
+function selectTopic() { 
 if (topic === 'HTML') {
     console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -16,5 +17,5 @@ if (topic === 'HTML') {
     console.log ("Let's study JavaScript!");
 } else {
     console.log('Please try again!');
+    }
 }
-*/
